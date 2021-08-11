ad9538720ce9ec91dff7a2808f5843216a9c7a43
cnava2804 <aristides2804@gmail.com>
2021-08-11 02:27:09 -0600

    Inicio del proyecto

---
+++ Proyecto_SI_Registro_Hotelero/Proyecto_SI_Registro_Hotelero/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proyecto_SI_Registro_Hotelero.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
