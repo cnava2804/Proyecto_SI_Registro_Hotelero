f6d179bc2aade71c5b76dcb884a548b6ae194d9b
JaviUnah <javiunah2020@gmail.com>
2021-08-11 22:58:04 -0600

    Creacion de clase de prueba

---
+++ Proyecto_SI_Registro_Hotelero/Proyecto_SI_Registro_Hotelero/Models/Prueba.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_SI_Registro_Hotelero.Models
+{
+    public class Prueba
+    {
+    }
+}
