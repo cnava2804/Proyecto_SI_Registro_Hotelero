f7d67b5aa05f8eaa792c081b7f9909530b29a3cd
OsirisSL <sanabria1997.19@gmail.com>
2021-08-17 00:00:21 -0600

    Actualizacion de proyecto

--- Proyecto_SI_Registro_Hotelero/Proyecto_SI_Registro_Hotelero/Migrations/20210816045334_Create.Designer.cs
+++ Proyecto_SI_Registro_Hotelero/Proyecto_SI_Registro_Hotelero/Migrations/20210816045334_Create.Designer.cs
@@ -10,8 +10,8 @@ using Proyecto_SI_Registro_Hotelero.Data;
 namespace Proyecto_SI_Registro_Hotelero.Migrations
 {
     [DbContext(typeof(PRHoteleroDbContext))]
-    [Migration("20210816043042_Update table PagoReserva")]
-    partial class UpdatetablePagoReserva
+    [Migration("20210816045334_Create")]
+    partial class Create
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
